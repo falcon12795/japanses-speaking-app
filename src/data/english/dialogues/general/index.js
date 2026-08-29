f19b70c46632ef2d25c9229d8055de241cfb8296
Falcon Black <blackfalcon9x@gmail.com>
2026-08-29 15:22:37 +0700

    add general diagloue

---
+++ src/data/english/dialogues/general/index.js
@@ -0,0 +1,9 @@
+import gen_01 from "./gen_01";
+import gen_02 from "./gen_02";
+import gen_03 from "./gen_03";
+
+export const EN_GENERAL = [
+    ...gen_01,
+    ...gen_02,
+    ...gen_03,
+]
--- src/data/english/dialogues/index.js
+++ src/data/english/dialogues/index.js
@@ -1,12 +1,15 @@
 import { EN_A1_DIALOGUES } from "./a1";
 import { EN_A2_DIALOGUES } from "./a2";
+import { EN_GENERAL } from "./general";
 
 export const EN_DIALOGUES = [
   ...EN_A1_DIALOGUES,
   ...EN_A2_DIALOGUES,
+  ...EN_GENERAL,
 ];
 
 export const EN_DIALOGUES_BY_LEVEL = {
   A1: EN_A1_DIALOGUES,
   A2: EN_A2_DIALOGUES,
+  GENERAL: EN_GENERAL,
 };
